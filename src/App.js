6ca03bd2bd01a84ed8462c76247b014bd470a44c
Pablo Winck Winter <pablowinck123@gmail.com>
2021-07-07 22:59:56 -0300

    Penúltima aula do primeiro curso de react

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React from "react";
-import ListaDeNotas from "./components/ListaDeNotas.jsx";
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+import "./assets/App.css";
+import './assets/index.css';
+class App extends Component {
 
-function App() {
-  return (
-    <section>
-      <form>
-        <input type="text" placeholder="Título" />
-        <textarea placeholder="Escreva sua nota..." />
-        <button>Criar Nota</button>
-      </form>
-      <ListaDeNotas/>
-    </section>
-  );
+  criarNota(titulo, texto){
+    console.log(`uma nova nota foi criada ${titulo} ${texto}`);
+  }
+  render() {
+    return (
+      <section className="conteudo">
+        <FormularioCadastro criarNota={this.criarNota}/>
+        <ListaDeNotas />
+      </section>
+    );
+  }
 }
 
 export default App;
