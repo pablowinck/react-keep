8994414872280a4fbfb9fe7fbedff6d331cf9ef7
Pablo Winck Winter <pablowinck123@gmail.com>
2021-07-12 10:37:50 -0300

    edit css

--- src/components/CardNota/CardNota.jsx
+++ src/components/CardNota/CardNota.jsx
@@ -13,8 +13,9 @@ class CardNota extends Component {
       <section className="card-nota">
         <header className="card-nota_cabecalho">
           <h3 className="card-nota_titulo">{this.props.titulo}</h3>
-          <DeleteSVG onClick={this.apagar.bind(this)}/>
-          <h4>{this.props.categoria}</h4>
+          <h4 className="card-nota_categoria">{this.props.categoria}</h4>
+          <DeleteSVG className="card-nota_deletesvg" onClick={this.apagar.bind(this)}/>
+
         </header>
         <p className="card-nota_texto">{this.props.texto}</p>
 
